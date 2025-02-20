030afe72fd4064982a9a557cafc1a76fded4ada7
Matthew Wilding <mbwilding@gmail.com>
2025-02-21 00:55:50 +0800

    2025-02-21-00-55-50

--- keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
+++ keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
@@ -27,8 +27,8 @@ enum layers {
     BASE,
     SYMB,
     NUMB,
-    GAME,
     MISC,
+    GAME,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,15 +59,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS
     ),
 
-    [GAME] = LAYOUT(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,             KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(BASE),
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,             KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
-        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,             KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, _______,
-        KC_LSFT, KC_Z   , KC_C   , KC_X   , KC_V   , KC_B   ,             KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
-
-                                            KC_SPC , KC_ENT ,             _______, _______
-    ),
-
     [MISC] = LAYOUT(
         _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______,
@@ -75,5 +66,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______,
 
                                             KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS
+    ),
+
+    [GAME] = LAYOUT(
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,             KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(BASE),
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,             KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
+        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,             KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, _______,
+        KC_LSFT, KC_Z   , KC_C   , KC_X   , KC_V   , KC_B   ,             KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
+
+                                            KC_SPC , KC_ENT ,             _______, _______
     ),
 };
