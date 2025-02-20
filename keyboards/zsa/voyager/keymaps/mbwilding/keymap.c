20a748a80645e8b5578ff8d9658912167ec9db44
Matthew Wilding <mbwilding@gmail.com>
2025-02-21 00:59:17 +0800

    2025-02-21-00-59-16

--- keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
+++ keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
@@ -34,7 +34,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, TO(GAME),
-        KC_DEL , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   ,             KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , S(KC_2),
+        KC_DEL , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   ,             KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , S_AT,
         KC_BSPC, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,             KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_MINS,
         S_EXCLA, KC_SCLN, KC_Q   , KC_J   , KC_K   , KC_X   ,             KC_B   , KC_M   , KC_W   , KC_V   , KC_Z   , S_QUEST,
 
