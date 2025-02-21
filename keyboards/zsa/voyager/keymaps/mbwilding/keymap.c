a0977323d258ccf9f8097883b466acf67cfbf1e8
Matthew Wilding <mbwilding@gmail.com>
2025-02-21 12:07:05 +0800

    2025-02-21-12-07-05

--- keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
+++ keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
@@ -9,14 +9,14 @@
 
 extern rgb_config_t rgb_matrix_config;
 
-#define C_OFF {0, 0, 0}
-#define C_BLUE {169, 255, 255}
-#define C_CYAN {131, 255, 255}
-#define C_GREEN {74, 255, 255}
-#define C_LBLUE {152, 255, 255}
+#define C_OFF    {000, 000, 000}
+#define C_BLUE   {169, 255, 255}
+#define C_CYAN   {131, 255, 255}
+#define C_GREEN  {074, 255, 255}
+#define C_LBLUE  {152, 255, 255}
 #define C_PURPLE {188, 255, 255}
-#define C_RED {0, 255, 255}
-#define C_YELLOW {41, 255, 255}
+#define C_RED    {000, 255, 255}
+#define C_YELLOW {041, 255, 255}
 
 enum custom_keycodes {
     RGB_SLD = ML_SAFE_RANGE,
