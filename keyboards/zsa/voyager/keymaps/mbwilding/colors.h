a476f392fde080c0d20331fa7114e082d2948b8b
Matthew Wilding <mbwilding@gmail.com>
2025-02-21 14:37:52 +0800

    2025-02-21-14-37-51

--- keyboards/zsa/voyager/keymaps/mbwilding/colors.h
+++ keyboards/zsa/voyager/keymaps/mbwilding/colors.h
@@ -1,8 +1,12 @@
-#define C_OFF    {   0,   0,   0 }
-#define C_BLUE   { 169, 255, 255 }
-#define C_CYAN   { 131, 255, 255 }
-#define C_GREEN  { 074, 255, 255 }
-#define C_LBLUE  { 152, 255, 255 }
-#define C_PURPLE { 188, 255, 255 }
-#define C_RED    {   0, 255, 255 }
-#define C_YELLOW { 041, 255, 255 }
+#define C_OFF      {   0,   0,   0 }
+#define C_BLUE     { 169, 255, 255 }
+#define C_CYAN     { 131, 255, 255 }
+#define C_GREEN    { 074, 255, 255 }
+#define C_LBLUE    { 152, 255, 255 }
+#define C_PURPLE   { 188, 255, 255 }
+#define C_RED      {   0, 255, 255 }
+#define C_YELLOW   { 041, 255, 255 }
+
+#define C_NUMB     C_GREEN
+#define C_FUNC     C_CYAN
+#define C_SYMB     C_RED
