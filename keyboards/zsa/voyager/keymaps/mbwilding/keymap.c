46244f04a00b0c71eed3bea4e75b0a96472ce655
Matthew Wilding <mbwilding@gmail.com>
2025-02-21 20:02:21 +0800

    2025-02-21-20-02-21

--- keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
+++ keyboards/zsa/voyager/keymaps/mbwilding/keymap.c
@@ -19,29 +19,37 @@ enum custom_keycodes {
 #include "colors.h"
 
 enum layers {
-    BASE,
-    SYMB,
-    NUMB,
-    MISC,
-    MIDI,
-    GAME,
+    L_BASE,
+    L_SYMB,
+    L_NUMB,
+    L_MIDI,
+    L_GAME,
+    // L_MISC,
 
     TOTAL_LAYERS,
 };
 
+enum tap_dances {
+    TD_ESC_DEL,
+};
+
 // Keys
 
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ESC_DEL] = ACTION_TAP_DANCE_DOUBLE(KC_ESCAPE, KC_DELETE),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_voyager(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          TO(MIDI),       TO(GAME),
-    KC_DELETE,      KC_QUOTE,       ALT_T(KC_COMM), GUI_T(KC_DOT),  CTL_T(KC_P),    KC_Y,                                           KC_F,           RCTL_T(KC_G),   RGUI_T(KC_C),   RALT_T(KC_R),   KC_L,           KC_AT,
-    KC_BSPC,        KC_A,           KC_O,           KC_E,           KC_U,           KC_I,                                           KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           KC_MINUS,
+  [L_BASE] = LAYOUT_voyager(
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          TO(L_MIDI),       TO(L_GAME),
+    KC_NO,          KC_QUOTE,       ALT_T(KC_COMM), GUI_T(KC_DOT),  CTL_T(KC_P),    KC_Y,                                           KC_F,           RCTL_T(KC_G),   RGUI_T(KC_C),   RALT_T(KC_R),   KC_L,           KC_AT,
+    KC_NO,          KC_A,           KC_O,           KC_E,           KC_U,           KC_I,                                           KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           KC_MINUS,
     KC_EXLM,        ALL_T(KC_SCLN), MEH_T(KC_Q),    KC_J,           KC_K,           KC_X,                                           KC_B,           KC_M,           KC_W,           MEH_T(KC_V),    ALL_T(KC_Z),    KC_QUES,
 
-                                                                    SFT_T(KC_SPACE),LT(MISC, KC_ENTER),                             LT(NUMB, KC_TAB), LT(SYMB, KC_ESCAPE)
+                                                                    SFT_T(KC_SPC),  KC_BSPC,                                        LT(L_NUMB, TD(TD_ESC_DEL)), LT(L_SYMB, KC_ENT)
   ),
 
-  [SYMB] = LAYOUT_voyager(
+  [L_SYMB] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_GRAVE,       KC_AMPR,        KC_HASH,        KC_CIRC,        KC_NO,                                          KC_NO,          KC_DLR,         KC_ASTR,        KC_PERC,        KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_LABK,        KC_LBRC,        KC_LCBR,        KC_LPRN,        KC_SLASH,                                       KC_BSLS,        KC_RPRN,        KC_RCBR,        KC_RBRC,        KC_RABK,        KC_NO,
@@ -50,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 
-  [NUMB] = LAYOUT_voyager(
+  [L_NUMB] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_NO,                                          KC_NO,          KC_7,           KC_8,           KC_9,           KC_NO,          KC_NO,
     KC_TRANSPARENT, KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_NO,                                          KC_NO,          KC_4,           KC_5,           KC_6,           KC_0,           KC_TRANSPARENT,
@@ -59,17 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 
-  [MISC] = LAYOUT_voyager(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-
-                                                                    KC_NO,          KC_NO,                                          KC_NO,          KC_NO
-  ),
-
-  [MIDI] = LAYOUT_voyager(
-    MI_TOGG,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          TO(BASE),
+  [L_MIDI] = LAYOUT_voyager(
+    MI_TOGG,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          TO(L_BASE),
     MI_Cs4,         MI_Ds4,         KC_NO,          KC_NO,          MI_Gs4,         MI_As4,                                         KC_NO,          MI_Cs5,         MI_Ds5,         KC_NO,          KC_NO,          MI_Gs5,
     MI_C4,          MI_D4,          MI_E4,          MI_F4,          MI_G4,          MI_A4,                                          MI_B4,          MI_C5,          MI_D5,          MI_E5,          MI_F5,          MI_G5,
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
@@ -77,24 +76,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     MI_OCTD,        MI_TRSD,                                        MI_TRSU,        MI_OCTU
   ),
 
-  [GAME] = LAYOUT_voyager(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TO(BASE),
+  [L_GAME] = LAYOUT_voyager(
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TO(L_BASE),
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
     KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT,
     KC_LEFT_SHIFT,  KC_Z,           KC_C,           KC_X,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT,
 
                                                                     KC_SPACE,       KC_ENTER,                                       KC_NO,          KC_NO
   ),
+
+  // [L_MISC] = LAYOUT_voyager(
+  //   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+  //   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+  //   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+  //   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+  //
+  //                                                                   KC_NO,          KC_NO,                                          KC_NO,          KC_NO
+  // ),
 };
 
 // LED
 
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
-    [BASE] = {
+    [L_BASE] = {
         // Left Keys
         C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_SYSTEM, C_PUNCTU, C_PUNCTU, C_PUNCTU, C_LETTER, C_LETTER,
-        C_SYSTEM, C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
+        C_OFF,    C_PUNCTU, C_PUNCTU, C_PUNCTU, C_LETTER, C_LETTER,
+        C_OFF,    C_LETTER, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
         C_PUNCTU, C_PUNCTU, C_LETTER, C_LETTER, C_LETTER, C_LETTER,
 
         // Left Thumbs
@@ -110,7 +118,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         C_LAYER,  C_LAYER
     },
 
-    [SYMB] = {
+    [L_SYMB] = {
         // Left Keys
         C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
         C_OFF,    C_PUNCTU, C_SYMBOL, C_SYMBOL, C_SYMBOL, C_OFF,
@@ -130,7 +138,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         C_LAYER,  C_LAYER
     },
 
-    [NUMB] = {
+    [L_NUMB] = {
         // Left Keys
         C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
         C_OFF,    C_FKEY,   C_FKEY,   C_FKEY,   C_FKEY,   C_OFF,
@@ -150,27 +158,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         C_LAYER,  C_LAYER
     },
 
-    [MISC] = {
-        // Left Keys
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-
-        // Left Thumbs
-        C_OFF,    C_OFF,
-
-        // Right Keys
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-        C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
-
-        // Right Thumbs
-        C_OFF,    C_OFF
-    },
-
-    [MIDI] = {
+    [L_MIDI] = {
         // Left Keys
         C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
         C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
@@ -190,7 +178,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         C_OFF,    C_OFF
     },
 
-    [GAME] = {
+    [L_GAME] = {
         // Left Keys
         C_SYSTEM, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER, C_NUMBER,
         C_SYSTEM, C_LETTER, C_WSAD  , C_LETTER, C_LETTER, C_LETTER,
@@ -209,6 +197,26 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         // Right Thumbs
         C_OFF,   C_OFF
     },
+
+    // [L_MISC] = {
+    //     // Left Keys
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //
+    //     // Left Thumbs
+    //     C_OFF,    C_OFF,
+    //
+    //     // Right Keys
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //     C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,    C_OFF,
+    //
+    //     // Right Thumbs
+    //     C_OFF,    C_OFF
+    // },
 };
 
 // Funcs
