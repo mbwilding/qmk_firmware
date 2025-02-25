2158f031a4cbfa984f6016ec96330480c55f9055
Matthew Wilding <mbwilding@gmail.com>
2025-02-26 01:00:25 +0800

    2025-02-26-01-00-25

---
+++ keyboards/zsa/voyager/keymaps/mbwilding/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 ZSA Technology Labs, Inc <@zsa>
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#define TAPPING_TERM 144
+#define QUICK_TAP_TERM 0
+#define FORCE_NKRO
+#define MIDI_ADVANCED
