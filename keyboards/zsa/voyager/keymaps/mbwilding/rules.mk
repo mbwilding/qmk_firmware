7aaa10ef0b069219163147cd7e2e1fa146c91328
Matthew Wilding <mbwilding@gmail.com>
2025-02-26 12:26:45 +0800

    2025-02-26-12-26-45

--- keyboards/zsa/voyager/keymaps/mbwilding/rules.mk
+++ keyboards/zsa/voyager/keymaps/mbwilding/rules.mk
@@ -1,12 +1,12 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = no
+
 COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+MIDI_ENABLE = yes
+MOUSEKEY_ENABLE = no
 MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
-
 TAP_DANCE_ENABLE = no
-MOUSEKEY_ENABLE = no
-MIDI_ENABLE = yes
