e0cad65b56b3e2d33923ea549ffb41d335425b62
Matthew Wilding <mbwilding@gmail.com>
2025-02-26 12:15:35 +0800

    2025-02-26-12-15-35

--- keyboards/zsa/voyager/keymaps/mbwilding/config.h
+++ keyboards/zsa/voyager/keymaps/mbwilding/config.h
@@ -1,17 +1,18 @@
-// Copyright 2023 ZSA Technology Labs, Inc <@zsa>
-// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
-// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
 
-#pragma once
-
-#define USB_SUSPEND_WAKEUP_DELAY 0
-#define RGB_MATRIX_STARTUP_SPD 60
+#undef TAPPING_TERM
+#define TAPPING_TERM 144
 
 #define PERMISSIVE_HOLD
-#define TAPPING_TERM 144
-#define QUICK_TAP_TERM 0
-#define FORCE_NKRO
-#define MIDI_ADVANCED
-
-// Need to configure
 // #define PERMISSIVE_HOLD_PER_KEY
+
+#define QUICK_TAP_TERM 0
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "p67KD/m553QV"
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
--- keyboards/zsa/voyager/keymaps/mbwilding/rules.mk
+++ keyboards/zsa/voyager/keymaps/mbwilding/rules.mk
@@ -1,8 +1,12 @@
-ORYX_ENABLE = yes
-TAP_DANCE_ENABLE = no
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-SPACE_CADET_ENABLE = no
 MOUSEKEY_ENABLE = no
+ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
+SPACE_CADET_ENABLE = no
+
+TAP_DANCE_ENABLE = no
+MOUSEKEY_ENABLE = no
 MIDI_ENABLE = yes
