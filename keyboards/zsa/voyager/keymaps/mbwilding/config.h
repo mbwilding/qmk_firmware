f64090c09ee39acce82abd607d4ab3a977c7edfb
Matthew Wilding <mbwilding@gmail.com>
2025-02-27 00:22:29 +0800

    2025-02-27-00-22-29

--- keyboards/zsa/voyager/keymaps/mbwilding/config.h
+++ keyboards/zsa/voyager/keymaps/mbwilding/config.h
@@ -3,10 +3,10 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define TAPPING_TERM 200
-#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 200 // How long a press takes before hold
+#define QUICK_TAP_TERM 100 // Double tap key repeat
 // #define QUICK_TAP_TERM_PER_KEY
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 // #define PERMISSIVE_HOLD_PER_KEY
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
