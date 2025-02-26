054f9210ca71fd2304d31873e6ca173145148a2b
Matthew Wilding <mbwilding@gmail.com>
2025-02-26 22:43:16 +0800

    2025-02-26-22-43-16

--- keyboards/zsa/voyager/keymaps/mbwilding/config.h
+++ keyboards/zsa/voyager/keymaps/mbwilding/config.h
@@ -3,17 +3,13 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#undef TAPPING_TERM
-#define TAPPING_TERM 144
-
-#undef QUICK_TAP_TERM
-#define QUICK_TAP_TERM 0
-
 #define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD_PER_KEY
+#define TAPPING_TERM 144
+#define QUICK_TAP_TERM 0
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "p67KD/m553QV"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
-// #define PERMISSIVE_HOLD_PER_KEY
